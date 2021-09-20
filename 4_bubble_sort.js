394f12f74b1c9be4afa0bb053fb99f073c40b9d4
Maria Antares <57846761+antaresofficial@users.noreply.github.com>
2021-09-20 21:17:31 +0500

    Update 4_bubble_sort.js

--- 4_bubble_sort.js
+++ 4_bubble_sort.js
@@ -3,8 +3,8 @@ let count = 0;
 
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      if (array[j + i] < array[j]) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j + 1] < array[j]) {
         let tmp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = tmp;
