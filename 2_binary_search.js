a0ed779fe2f7b88a42bda042127220ad774d7a71
Maria Antares <57846761+antaresofficial@users.noreply.github.com>
2021-09-21 09:38:15 +0500

    Update 2_binary_search.js

--- 2_binary_search.js
+++ 2_binary_search.js
@@ -1,10 +1,29 @@
-const array = [1, 2, 3, 4, 5, 7, 8, 9];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let count = 0;
+
+function binarySearchTest(array, item) {
+  let start = 0;
+  let end = array.length;
+
+  while (start <= end) {
+    count += 1;
+    let middle = Math.floor((start + end) / 2);
+    if (array[middle] === item) {
+      return middle;
+    }
+
+    if (item < array[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+}
 
 function binarySearch(array, item) {
   for (let i = 0; i < array.length; i++) {
-    let start = 0;
-    let end = array.length;
+    let start = 0; // 0
+    let end = array.length; // 9
     let middle;
     let found = false;
     let position = -1;
@@ -28,5 +47,5 @@ function binarySearch(array, item) {
   return position;
 }
 
-console.log(binarySearch(array, 1));
+console.log(binarySearchTest(array, 8));
 console.log(`count: ${count}`);
